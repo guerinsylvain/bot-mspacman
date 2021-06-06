de20634c1c3797075cece14b337bb169b60c3fa0
sguerin <sguerin@guess-engineering.com>
2021-06-06 14:31:09 +0200

    episode loop added

--- train.py
+++ train.py
@@ -1,7 +1,23 @@
 import gym
+
+# parameters
+num_episodes = 10
+
 env = gym.make('MsPacman-v0')
-env.reset()
-for _ in range(10000):
-    env.render()
-    env.step(env.action_space.sample())  # take a random action
+print("actions [0-8]:", env.env.get_action_meanings())
+history = []
+
+for i in range(num_episodes):
+    done = False
+    episodic_reward = 0
+    env.reset()
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        next_obs, reward, done, _  = env.step(action)  
+        episodic_reward += reward
+    
+    history.append(episodic_reward)
+    print("Episode number:", i, 'Episode Reward:', episodic_reward)
+print(history)
 env.close()
