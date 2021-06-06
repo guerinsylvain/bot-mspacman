c98f634cadadc84d7f9c7f53e9d418b0d404e08a
sguerin <sguerin@guess-engineering.com>
2021-06-06 19:22:56 +0200

    experience and replay memory added

---
+++ experience.py
@@ -0,0 +1,7 @@
+class Experience:
+    def __init__(self, intial_obs, action, reward, next_obs, done):
+        self.intial_obs = intial_obs
+        self.action = action
+        self.reward = reward
+        self.next_obs = next_obs
+        self.done = done
